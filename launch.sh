4718166f056baa23d3224d10fbb89e098a38bb30
CyferekCyfur <kubapro84@gmail.com>
2024-03-18 14:33:33 +0100

    fixed running script

--- launch.sh
+++ launch.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
-export DISPLAY=localhost:0.0
-xterm -e -hold roslaunch jetracer jetracer.launch 
-roscore
+nohup roscore &
 
+nohup roslaunch jetracer jetracer.launch &
+
+roscore_pid=$!
+jetracer_pid=$!
+
+echo "PID roscore: $roscore_pid"
+echo "PID jetracer: $jetracer_pid"
 
